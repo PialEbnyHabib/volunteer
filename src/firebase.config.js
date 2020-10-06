73fc496162ae54593db906208e5c6dc6f3188182
pial ebny habib <khanmd.pialebnyhabib@gmail.com>
2020-10-06 23:35:30 +0600

    final

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKjxmiFgOYuJzt5wKOqULszN3uvrywByE",
+    authDomain: "volunter-network-54778.firebaseapp.com",
+    databaseURL: "https://volunter-network-54778.firebaseio.com",
+    projectId: "volunter-network-54778",
+    storageBucket: "volunter-network-54778.appspot.com",
+    messagingSenderId: "941196451043",
+    appId: "1:941196451043:web:040142493a56829fec79d6"
+  };
+
+  export default firebaseConfig; 
